15d56ee903a0eaabb89e1be4d32cd6b538ab7df6
Collin M <wingedillidan@gmail.com>
2015-09-21 15:57:22 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'A program to help figure out object pixel positions',
+    'author': 'Collin McLean',
+    'url': 'http://github.com/wingedillidan/PosTool',
+    'author_email': 'wingedillidan@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['PosTool'],
+    'scripts': [],
+    'name': 'PosTool'
+}
+
+setup(**config)
