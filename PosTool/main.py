4b2f772c38f85d936e6a14e28b510f44924592e7
Collin M <wingedillidan@gmail.com>
2015-09-23 00:12:14 -0400

    second attempt at position indication, it works this time!

--- PosTool/main.py
+++ PosTool/main.py
@@ -1,12 +1,13 @@
 import pygame
 import load
 import sys
-import pdb
 from screen import screen
 from pygame.locals import *
 
 
 class Entity(pygame.sprite.Sprite):
+    """A game object class for cases where objects need to be clicked,
+    dragged, and move based on cursor position"""
 
     def __init__(self, name, colorkey=(0, 0, 0)):
         pygame.sprite.Sprite.__init__(self)
@@ -14,24 +15,10 @@ class Entity(pygame.sprite.Sprite):
         self.original = self.image
         self.grabbed = False
         self.name = name
+
+        # where the cursor is in relation to the object's top left pixel
         self.dx = 0
         self.dy = 0
-
-        # font information
-        self.font = pygame.font.Font(None, 18)
-        self.font_surface = None
-        self.font_rect = None
-
-    def position(self):
-        """Displays the object's position information in the top left corner
-        of the object"""
-        if self.font_surface:
-            self.image.blit(self.original, self.font_rect, self.font_rect)
-
-        text = '{}, {}'.format(str(self.rect.x), str(self.rect.y))
-        self.font_surface = self.font.render(text, True, (255, 255, 255))
-        self.font_rect = self.font_surface.get_rect()
-        self.image.blit(self.font_surface, self.font_rect)
 
     def update(self):
         if self.grabbed:
@@ -39,17 +26,42 @@ class Entity(pygame.sprite.Sprite):
             offset = (pos[0]-self.dx, pos[1]-self.dy)
 
             self.rect.topleft = offset
-            self.position()
 
     def grab(self):
         self.grabbed = True
 
+        # dx, dy enable the object to be moved smoothly and without it
+        # jumping to a preset movement point (uhhh, do I need this comment?)
         pos = pygame.mouse.get_pos()
         self.dx = pos[0] - self.rect.x
         self.dy = pos[1] - self.rect.y
 
     def ungrab(self):
         self.grabbed = False
+
+
+class EntityPos(pygame.sprite.Sprite):
+    """Displays position information relative to the given reference object
+    in the form of text on the topright corner of the object."""
+
+    def __init__(self, gameobj):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.ref = gameobj
+        self.font = pygame.font.Font(None, 18)
+        self.image = self.font.render('???', True, (0, 0, 0))
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        # set coloring
+        background = (0, 0, 0)
+        color = (255, 255, 255)
+
+        # update & position
+        x, y = self.ref.rect.x, self.ref.rect.y
+        text = '{}, {}'.format(x, y)
+        self.image = self.font.render(text, True, color, background)
+        self.rect.x, self.rect.y = x, y
 
 
 # load the icon to display up on the top left
@@ -66,6 +78,7 @@ test1 = Entity('button_unpressed_green-240x60.bmp')
 test2 = Entity('menu.bmp')
 test3 = Entity('button_unpressed_red-52x60.bmp')
 testgroup = pygame.sprite.RenderClear((test1, test2, test3))
+testgrouppos = pygame.sprite.RenderClear()
 clock = pygame.time.Clock()
 
 # FPS info
@@ -76,22 +89,35 @@ if pygame.font:
     fps_surf = fps_font.render('FPS: ???', True, (0, 0, 0))
     fps_rect = fps_surf.get_rect()
 
+# main loop
 while True:
     clock.tick(60)
 
     for e in pygame.event.get():
+        # did a thing get grabbed?
         if e.type == MOUSEBUTTONDOWN and e.button == 1:
             pos = pygame.mouse.get_pos()
 
+            # check which object got grabbed
+            # TO DO: fix grabbing to select the topmost object first
             for thing in testgroup.sprites():
                 if thing.rect.collidepoint(pos):
                     thing.grab()
+                    testgrouppos.add(EntityPos(thing))
                     break
+
+        # test case for removing stuff
+        # will break if clicked 4 times
         elif e.type == MOUSEBUTTONDOWN and e.button == 3:
             testgroup.remove(testgroup.sprites()[0])
+
+        # did grabbing stop?
         elif e.type == MOUSEBUTTONUP:
             for thing in testgroup.sprites():
                 thing.ungrab()
+                testgrouppos.empty()
+
+        # quit cases
         elif e.type == KEYUP and e.key == K_ESCAPE:
             sys.exit()
         elif e.type == QUIT:
@@ -99,16 +125,20 @@ while True:
 
     # clear necessary wizzles
     testgroup.clear(screen, background)
+    testgrouppos.clear(screen, background)
     screen.blit(background, fps_rect, fps_rect)
 
     # update the shizzle wizzles
     testgroup.update()
+    testgrouppos.update()
     fps_surf = fps_font.render('FPS: ' + str(int(clock.get_fps())),
                                True, (0, 0, 0))
     fps_rect = fps_surf.get_rect()
 
     # redraw the shizzles
     testgroup.draw(screen)
+    testgrouppos.draw(screen)
     screen.blit(fps_surf, fps_rect)
 
+    # update
     pygame.display.update()
